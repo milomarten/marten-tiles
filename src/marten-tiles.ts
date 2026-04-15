55dbaaae0929c38e527970391e2c43005576945e
Milo Marten <milomartenstreams@gmail.com>
2026-04-14 20:40:04 -0500

    Removing all that stupid hello world stuff

--- src/marten-tiles.ts
+++ src/marten-tiles.ts
@@ -1,34 +1,15 @@
 import { LitElement, html, css } from 'lit';
 import { property, customElement } from 'lit/decorators.js';
 
-const logo = new URL('../../assets/open-wc-logo.svg', import.meta.url).href;
-
 @customElement('marten-tiles')
 export class MartenTiles extends LitElement {
-  @property({ type: String }) header = 'My app';
+  // @property({ type: String }) header = 'My app';
 
   static styles = css`
-    :host {
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: flex-start;
-      font-size: calc(10px + 2vmin);
-      color: #1a2b42;
-      max-width: 960px;
-      margin: 0 auto;
-      text-align: center;
-      background-color: var(--marten-tiles-background-color);
-    }
-
-    main {
-      flex-grow: 1;
-    }
-
     .logo {
       margin-top: 36px;
       animation: app-logo-spin infinite 20s linear;
+      width: fit-content;
     }
 
     @keyframes app-logo-spin {
@@ -39,43 +20,13 @@ export class MartenTiles extends LitElement {
         transform: rotate(360deg);
       }
     }
-
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
-      align-items: center;
-    }
-
-    .app-footer a {
-      margin-left: 5px;
-    }
   `;
 
   render() {
     return html`
       <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
-        <h1>${this.header}</h1>
-
-        <p>Edit <code>src/MartenTiles.ts</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
+        <p class="logo">Hello, World!</p>
       </main>
-
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
     `;
   }
 }
